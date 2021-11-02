3554673e24d6e98acf136dff7008b5107c8cfbf7
Simon_Ituen <ituensimonituen@gmail.com>
2021-11-02 15:26:46 +0100

    Phase 3 new translations

--- lib/screens/home/HomeScreen.dart
+++ lib/screens/home/HomeScreen.dart
@@ -1415,7 +1415,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                   ],
                                 )),
                           ),*/
-                          Padding(
+                          if(false)Padding(
                             padding: const EdgeInsets.symmetric(
                                 vertical: 16.0, horizontal: 16),
                             child: Text(
@@ -1426,7 +1426,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                   fontWeight: FontWeight.w500),
                             ),
                           ),
-                          SingleChildScrollView(
+                          if(false)SingleChildScrollView(
                             scrollDirection: Axis.horizontal,
                             padding: EdgeInsets.symmetric(horizontal: 8),
                             child: currentCarsPage == 0
@@ -1577,7 +1577,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                     ],
                                   ),
                           ),
-                          Visibility(
+                          if(false)Visibility(
                             visible: _appProvider.isLoggedIn &&
                                 (currentCarsPage == 0
                                     ? _appProvider
@@ -1598,7 +1598,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               ),
                             ),
                           ),
-                          Visibility(
+                          if(false)Visibility(
                             visible: _appProvider.isLoggedIn,
                             child: SingleChildScrollView(
                               scrollDirection: Axis.horizontal,
